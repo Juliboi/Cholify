4df6258b823fe5a8df09821d26d4b5a07e04f816
Julian Xu <xuj@avast.com>
2022-02-11 15:52:59 +0100

    new features (searchbar, fixed auth, filter)

---
+++ cholify-build/cholify-app/precache-manifest.e57384602e0623bf5ea332216b520412.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b62b5bdb3edfeacea4194cb4a8982fe1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bcb1ab632747fd0dfa87",
+    "url": "./static/css/main.875a7905.chunk.css"
+  },
+  {
+    "revision": "2e9c3131db99e3be937e",
+    "url": "./static/js/2.ca7b3ee7.chunk.js"
+  },
+  {
+    "revision": "3f0d459f7371750fd047ea2df883a2f4",
+    "url": "./static/js/2.ca7b3ee7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcb1ab632747fd0dfa87",
+    "url": "./static/js/main.962ac503.chunk.js"
+  },
+  {
+    "revision": "dd3caa90e0a0fa0521cce903f73bd6a8",
+    "url": "./static/js/main.962ac503.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6497363f5e478feb7e40",
+    "url": "./static/js/runtime-main.4ffa4d21.js"
+  },
+  {
+    "revision": "5fb748c00e0fc8372a316dfe242fd2d1",
+    "url": "./static/media/cholify-web-logo.5fb748c0.svg"
+  }
+]);
